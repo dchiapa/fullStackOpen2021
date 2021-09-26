c1d5260e4171d2702c988a27ce381d85b4dde649
Diego <dchiapa@est.frba.utn.edu.ar>
2021-09-26 20:40:42 -0300

    add exercise 1.8

--- part_01/unicafe/src/index.js
+++ part_01/unicafe/src/index.js
@@ -1,45 +1,44 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+const Feedback = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positive = (good / total) * 100;
+  return (
+    <>
+      <p>
+        <strong>Feedback:</strong>
+      </p>
+      <p>
+        <strong>Total:</strong> {total}
+      </p>
+      <p>
+        <strong>Average:</strong> {average}
+      </p>
+      <p>
+        <strong>Positive:</strong> {positive} %
+      </p>
+    </>
+  );
+};
+const Statistics = ({ good, neutral, bad }) => (
+  <>
+    <p>Good: {good}</p>
+    <p>Neutral: {neutral}</p>
+    <p>Bad: {bad}</p>
+    <Feedback good={good} neutral={neutral} bad={bad} />
+  </>
+);
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  const Button = ({ handleClick, text }) => (
-    <button onClick={handleClick}>{text}</button>
-  );
-
-  const Statistics = ({ good, neutral, bad }) => (
-    <>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <Feedback good={good} neutral={neutral} bad={bad} />
-    </>
-  );
-  const Feedback = ({ good, neutral, bad }) => {
-    const total = good + neutral + bad;
-    const average = (good - bad) / total;
-    const positive = (good / total) * 100;
-    return (
-      <>
-        <p>
-          <strong>Feedback:</strong>
-        </p>
-        <p>
-          <strong>Total:</strong> {total}
-        </p>
-        <p>
-          <strong>Average:</strong> {average}
-        </p>
-        <p>
-          <strong>Positive:</strong> {positive} %
-        </p>
-      </>
-    );
-  };
 
   return (
     <main>
