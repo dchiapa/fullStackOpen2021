5dc7b24f7b0e0a2a6656e1fa553828d86fdb2944
Diego <dchiapa@est.frba.utn.edu.ar>
2021-10-06 15:59:21 -0300

    add exercise 3.1 - backend phonebook

---
+++ part_03/phonebook_backend/index.js
@@ -0,0 +1,46 @@
+const { request, response } = require("express");
+const express = require("express");
+const app = express();
+
+let contacts = [
+  {
+    name: "Arto Hellas",
+    phone: "040-123456",
+    id: 1,
+  },
+  {
+    name: "Ada Lovelace",
+    phone: "39-44-5323523",
+    id: 2,
+  },
+  {
+    name: "Dan Abramov",
+    phone: "12-43-234345",
+    id: 3,
+  },
+  {
+    name: "Mary Poppendieck",
+    phone: "39-23-6423122",
+    id: 4,
+  },
+  {
+    name: "Diego",
+    phone: "1234",
+    id: 5,
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Phonebook</h1>");
+});
+
+app.get("/api/contacts", (request, response) => {
+  response.writeHead(200, { "Content-Type": "application/json" });
+  response.end(JSON.stringify(contacts));
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
